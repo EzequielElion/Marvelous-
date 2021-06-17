2d42f730a0c0149d0c870053f35b8f91b208ea4b
EzequielElion <84689505+EzequielElion@users.noreply.github.com>
2021-06-17 14:50:15 -0300

    Create using System;.cs

---
+++ using System;.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Marvel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("iniciando...")
+        }
+    }
+}
